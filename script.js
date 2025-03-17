6b4342bf147c5a5c584bf6c03fc11aa7e219399c
naelistheir <acerfarell@gmail.com>
2025-03-18 02:44:06 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const btn = document.querySelector(".btn");
-    btn.addEventListener("mouseover", function() {
-        btn.style.transform = "scale(1.1)";
-    });
-    btn.addEventListener("mouseout", function() {
-        btn.style.transform = "scale(1)";
-    });
-});
