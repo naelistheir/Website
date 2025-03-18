13e4243a841fb82a58efff5ffdd3f1128da24d28
naelistheir <acerfarell@gmail.com>
2025-03-19 04:37:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+window.onload = function() {
+    document.getElementById("popup").style.display = "flex";
+};
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     const elements = document.querySelectorAll(".animasi-teks");
 
